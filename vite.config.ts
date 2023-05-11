e4bd1c0b1b3732109ce482ce60b7728fac244af7
sugar(슈가/김수란) <sugar.kim@kakaostyle.com>
2023-05-11 09:14:36 +0900

    문제되는 vite-plugin-next-react-router 라이브러리 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import { reactRouterPlugin } from 'vite-plugin-next-react-router';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRouterPlugin({
-    async: true,
-    pageDir: 'src/pages',
-    extensions: ['js', 'jsx', 'ts', 'tsx'],
-  }),
-  ],
+  plugins: [react()  ],
 })
